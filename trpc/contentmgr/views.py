8ca551d112cfbaee0948f35068383a7b2cc760ca
Patrick Conway <conway.pd@gmail.com>
2013-02-10 17:13:14 -0500

    handling static files, first templates

--- trpc/contentmgr/views.py
+++ trpc/contentmgr/views.py
@@ -1,7 +1,16 @@
 from contentmgr.models import Content
+from django.template import loader, Context, RequestContext
 from django.http import HttpResponse
 
 def home(request):
     contents = Content.objects.all().order_by('-pub_date')
-    output = '<p></p>'.join([c.get_html() for c in contents])
+    c = RequestContext(request, { 'content_list' : contents } )
+    t = loader.get_template('index.html')
+    output = t.render(c)
     return HttpResponse(output)
+
+def detail(request, content_id):
+    return HttpResponse("HI")
+
+def edit(request, content_id):
+    return HttpResponse("EDIT")
--- trpc/trpc/urls.py
+++ trpc/trpc/urls.py
@@ -5,9 +5,9 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'contentmgr.views.home'),
-    url(r'^view/(?P<content_id>\d+)/$', 'contentmgr.views.detail'),
-    url(r'^edit/(?P<content_id>\d+)/$', 'contentmgr.views.edit')
+    url(r'^$', 'contentmgr.views.home', name='home'),
+    url(r'^view/(?P<content_id>\d+)/$', 'contentmgr.views.detail', name='detail'),
+    url(r'^edit/(?P<content_id>\d+)/$', 'contentmgr.views.edit', name='edit')
     # Examples:
     # url(r'^$', 'trpc.views.home', name='home'),
     # url(r'^trpc/', include('trpc.foo.urls')),
